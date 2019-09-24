286c2301262c15df78bc6092dbb29a315f2549a4
gucci.ku <gucci.ku@csnt.com.tw>
2019-09-24 09:57:22 +0800

    remove code.

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
 import {GraphQLServer} from 'graphql-yoga'
 import typeDefs from './types/typeDefs'
-import {prisma} from './server/generated/prisma-client'
 import {resolvers} from './resolvers'
-// const resolvers = {
-//   Query: {
-//     users: (_,) => {
-//       return prisma.users()
-//     }
-//   },
-//   Mutation: {
-//     createUser: (_, {name}) => {
-//       return prisma.createUser({
-//         name: name,
-//       })
-//     },
-//     deleteUser: (_, {id}) => {
-//       return prisma.deleteUser({
-//         id: id,
-//       })
-//     },
-//   },
-//   //訂閱 即時消息
-//   // Subscriptions: {
-//   // }
-// }
 
 //解析器 數值來源
 const server = new GraphQLServer({typeDefs, resolvers})
